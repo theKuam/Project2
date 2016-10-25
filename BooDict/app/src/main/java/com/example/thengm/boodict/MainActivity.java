c77448c8a64043f1e9af929548e6dd7761c53d9a
theKuam <ngominh.hai96@gmail.com>
2016-10-25 15:42:49 +0700

    Project2

--- BooDict/app/src/main/java/com/example/thengm/boodict/MainActivity.java
+++ BooDict/app/src/main/java/com/example/thengm/boodict/MainActivity.java
@@ -2,18 +2,31 @@ package com.example.thengm.boodict;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mData;
+    Button btNetwork;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mData = FirebaseDatabase.getInstance().getReference();
+        btNetwork = (Button) findViewById(R.id.network);
 
-        mData.setValue("123");
+        btNetwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Controller.isNetworkConneceted(MainActivity.this, MainActivity.this)==false){
+                    Toast.makeText(MainActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+                };
+            }
+        });
+
     }
 }
