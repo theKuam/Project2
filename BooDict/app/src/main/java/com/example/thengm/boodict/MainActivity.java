782667d01307b26a1c713fa82176615057a94695
theKuam <ngominh.hai96@gmail.com>
2016-10-25 14:55:30 +0700

    Project2

---
+++ BooDict/app/src/main/java/com/example/thengm/boodict/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.thengm.boodict;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mData = FirebaseDatabase.getInstance().getReference();
+
+        mData.setValue("123");
+    }
+}
